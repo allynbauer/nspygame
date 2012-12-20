8c1902dcb5628b1dc5b8af2e7889e06a0fe22eb5
Allyn Bauer <allyn.bauer@gmail.com>
2012-12-19 22:57:56 -0600

    comments

--- event_responder.py
+++ event_responder.py
@@ -3,16 +3,19 @@ import pygame
 class EventResponder(object):
 	def __init__(self, callback):
 		self.callback = callback
+		assert callable(callback) "callback must be callable"
 		self.type = None
 
 	def responds_to_event(self, event):
 		return event.type == self.type
 
+# calls callback with the event when the mouse moves
 class MouseMotionResponder(EventResponder):
 	def __init__(self, callback):
 		EventResponder.__init__(self, callback)
 		self.type = pygame.MOUSEMOTION
 
+# calls callback with the event when button is clicked
 class MouseButtonDownResponder(EventResponder):
 	def __init__(self, callback):
 		EventResponder.__init__(self, callback)
@@ -22,6 +25,7 @@ class MouseButtonDownResponder(EventResponder):
 	def responds_to_event(self, event):
 		return EventResponder.responds_to_event(self, event) and self.button == event.button
 
+# calls callback with the event when button is released
 class MouseButtonUpResponder(EventResponder):
 	def __init__(self, callback):
 		EventResponder.__init__(self, callback)
@@ -44,8 +48,8 @@ class KeyEventResponder(EventResponder):
 
 # actives when all keys are active on view. currently that view must be the window, because its hard
 # repeats_every is when the event repeats. positive value in seconds, zero means doesn't repeat until key up.
-# keys is a set of keys. but for now it will only support one
-# mods is a set of mods #http://www.pygame.org/docs/ref/key.html
+# keys is a list of keys. but for now it will only support one
+# mods is a list of mods #http://www.pygame.org/docs/ref/key.html
 class KeyDownResponder(KeyEventResponder):
 	def __init__(self, keys, mods, repeats_every, callback):
 		KeyEventResponder.__init__(self, keys, mods, repeats_every, callback)
